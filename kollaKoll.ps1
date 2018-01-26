a6cdb775d844060edc5187fcfe41a309ea2d1cc9
root <root@LOVDOT049.orebroll.se>
2018-01-26 13:07:05 +0100

    Added ignore

---
+++ kollaKoll.ps1
@@ -0,0 +1,41 @@
+﻿cls
+#$ErrorActionPreference = "Silentlycontinue"
+
+#OBS TESTA INTE KOLLHIST!
+#$computers = "kollslave1", "kollslave2","kollmaster" #"kollmaster","kollslave1", "kollslave2"#,"kollhist"
+
+$computers="kollhist"
+$sokfilter="hsaIdentity=SE2321000164-tal008"
+
+#ldap://kollhist.orebroll.se/o=Region Örebro län,l=Örebro län,c=SE,dc=history,c=SE och med ett sökfilter ”hsaIdentity=SE2321000164-tal008”
+#$sokfilter="sn=almén"
+
+foreach ($c in $computers) 
+{
+
+    $katalogroot = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$($c)/c=se","","","none")
+    $object=new-object DirectoryServices.DirectorySearcher($katalogroot,$sokfilter)
+    write-host -nonewline "Testar $c..."
+    
+    try{
+        $objresult=$object.FindAll()
+        
+        if ($objresult.path.Count -gt 0 )
+        {
+            write-output ("$($objresult.path.Count) träff(-ar)")
+            $traffar=""
+            $objresult.path | % { 
+                #Visar de träffar som returnerades
+                $traffar+="[$($psitem)]`n"
+            }
+            write-output $traffar`n
+        }
+        else { Write-output "[Inga träffar på '$($sokfilter)']" }  
+    }
+    Catch {
+        Write-output "[Fail: $($psitem.Exception.Message.replace("`n",''))]"
+    }
+$error.Clear()
+} 
+
+
