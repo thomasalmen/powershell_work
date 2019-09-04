92c3c34ac9e4b7af54e5ff9b9feb3d33ae75eb16
thomasalmen <32485448+thomasalmen@users.noreply.github.com>
2019-09-04 11:27:19 +0200

    Add files via upload

---
+++ F5/Kerberos_Delegation.ps1
@@ -0,0 +1,13 @@
+﻿New-ADUser -Name "F5-DELEGATION" -UserPrincipalName F5-SSO@scb.intra -SamAccountName "F5-DELEGATION" -PasswordNeverExpires $true -Enabled $true -AccountPassword (ConvertTo-SecureString -AsPlainText "P@ssw0rd" -Force) 
+
+#Fixa SPN 
+Set-AdUser -Identity F5-SSO -ServicePrincipalNames @{Add="host/F5-DELEGATIONHOST.supercow.se"}
+
+
+# Fixa delegationrättighet. I detta fall mot websajt testweb.xxx.se
+Get-AdUser -Identity F5-DELEGATION | Set-ADObject -Add @{"msDS-AllowedToDelegateTo"="http/testweb.xxx.se"}  
+ 
+
+# Sätt delegationsrättighet för denna specifia service 
+Set-ADAccountControl -Identity F5-DELEGATION -TrustedForDelegation $false 
+Set-ADAccountControl -Identity F5-DELEGATION -TrustedToAuthForDelegation $true  
