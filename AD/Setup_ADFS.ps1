30ca75f49883dbc5efb731d3a96ca40fc5ba8259
thomasalmen <32485448+thomasalmen@users.noreply.github.com>
2019-09-04 11:47:02 +0200

    Add files via upload

---
+++ AD/Setup_ADFS.ps1
@@ -0,0 +1,40 @@
+$domainName = "supercow.se"
+$password = "P@ssw0rd"
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$fqdn = [System.Net.Dns]::GetHostByName(($env:computerName)) | FL HostName | Out-String | %{ "{0}" -f $_.Split(':')[1].Trim() };
+$filename = "C:\$fdqn.pfx"
+$user = "supercow.se\administrator"
+$credential = New-Object `
+    -TypeName System.Management.Automation.PSCredential `
+    -ArgumentList $user, $securePassword
+
+Write-Host "Installing nuget package provider"
+Install-PackageProvider nuget -force
+
+Write-Host "Installing PSPKI module"
+Install-Module -Name PSPKI -Force
+
+Write-Host "Importing PSPKI into current environment"
+Import-Module -Name PSPKI
+
+Write-Host "Generating Certificate"
+$selfSignedCert = New-SelfSignedCertificateEx `
+    -Subject "CN=$fqdn" `
+    -ProviderName "Microsoft Enhanced RSA and AES Cryptographic Provider" `
+    -KeyLength 2048 -FriendlyName 'OAFED SelfSigned' -SignatureAlgorithm sha256 `
+    -EKU "Server Authentication", "Client authentication" `
+    -KeyUsage "KeyEncipherment, DigitalSignature" `
+    -Exportable -StoreLocation "LocalMachine"
+$certThumbprint = $selfSignedCert.Thumbprint
+
+Write-Host "Installing ADFS"
+Install-WindowsFeature -IncludeManagementTools -Name ADFS-Federation
+
+Write-Host "Configuring ADFS"
+Import-Module ADFS
+Install-AdfsFarm -CertificateThumbprint $certThumbprint -FederationServiceName $fqdn -ServiceAccountCredential $credential
+
+"Installing Ca feature.."
+Add-WindowsFeature adcs-cert-authority,RSAT-ADCS,RSAT-ADCS-mgmt
+Install-AdcsCertificationAuthority -CAType EnterpriseRootCa -CryptoProviderName "ECDSA_P256#Microsoft Software Key Storage Provider" -KeyLength 256 -HashAlgorithmName SHA256
+
